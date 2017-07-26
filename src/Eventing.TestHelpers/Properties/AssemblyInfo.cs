20dc71bb804227289f5ab05af57ee65740987197
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-07-26 14:51:34 -0400

    Fixed TestHelpers project

--- src/Eventing.TestHelpers/Properties/AssemblyInfo.cs
+++ src/Eventing.TestHelpers/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Eventing.TestHelpers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -11,10 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("c4ba7fa1-b2f9-4543-8013-5ecc3bed5b50")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("31913791-9e19-4f54-bc07-ef281988c0cd")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Eventing.TestHelpers/TestableEventSourcedService.cs
+++ src/Eventing.TestHelpers/TestableEventSourcedService.cs
@@ -99,3 +99,5 @@ namespace Eventing.TestHelpers
         }
     }
 }
+
+
