d78af597a4fb149f7cf13249bb8e5b24c86f29e7
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-09-22 10:34:13 -0400

    Fixed checkpRepo interface

--- src/Eventing.Core/Persistence/ICheckpointRepository.cs
+++ src/Eventing.Core/Persistence/ICheckpointRepository.cs
@@ -2,11 +2,12 @@
 
 namespace Eventing.Core.Persistence
 {
+    // This is just to give an idea of how to do it
     public interface ICheckpointRepository
     {
         long? GetCheckpoint(string subscriptionId);
         Task<long?> GetCheckpointAsync(string subscriptionId);
-        void SaveCheckpoint(long checkpoint);
-        Task SaveCheckpointAsync(long checkpoint);
+        void SaveCheckpoint(string subscriptionId, long checkpoint);
+        Task SaveCheckpointAsync(string subscriptionId, long checkpoint);
     }
 }
