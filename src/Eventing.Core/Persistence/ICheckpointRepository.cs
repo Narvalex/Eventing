558cde07f71aa1803bdf6cd7542d156e02644758
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-09-22 10:29:43 -0400

    Added persistent checkpointing capabilities

---
+++ src/Eventing.Core/Persistence/ICheckpointRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Eventing.Core.Persistence
+{
+    public interface ICheckpointRepository
+    {
+        long? GetCheckpoint(string subscriptionId);
+        Task<long?> GetCheckpointAsync(string subscriptionId);
+        void SaveCheckpoint(long checkpoint);
+        Task SaveCheckpointAsync(long checkpoint);
+    }
+}
