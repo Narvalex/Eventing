dff6b23d06cc9ac0d396e66c3aea1f5eb76bdef3
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2017-09-26 14:29:51 -0400

    Added usability overloads and an api to check if an stream exists

--- src/Eventing.Core/Domain/EventSourcedReaderExtensions.cs
+++ src/Eventing.Core/Domain/EventSourcedReaderExtensions.cs
@@ -8,8 +8,7 @@ namespace Eventing.Core.Domain
     {
         public static async Task<T> GetByIdAsync<T>(this IEventSourcedReader reader, string streamId) where T : class, IEventSourced, new()
         {
-            var streamName = StreamCategoryAttribute.GetFullStreamName<T>(streamId);
-            return await reader.GetAsync<T>(streamName);
+            return await reader.GetAsync<T>(GetStreamName<T>(streamId));
         }
 
         public static async Task<T> GetOrFailAsync<T>(this IEventSourcedReader reader, string streamName)
@@ -23,8 +22,25 @@ namespace Eventing.Core.Domain
         public static async Task<T> GetOrFailByIdAsync<T>(this IEventSourcedReader reader, string streamId)
            where T : class, IEventSourced, new()
         {
-            var streamName = StreamCategoryAttribute.GetFullStreamName<T>(streamId);
-            return await reader.GetOrFailAsync<T>(streamName);
+            return await reader.GetOrFailAsync<T>(GetStreamName<T>(streamId));
         }
+
+        public static async Task<bool> Exists<T>(this IEventSourcedReader reader, string streamId)
+        {
+            return await reader.Exists(GetStreamName<T>(streamId));
+        }
+
+        public static async Task EnsureExistence<T>(this IEventSourcedReader reader, string streamId)
+        {
+            await reader.EnsureExistence(GetStreamName<T>(streamId));
+        }
+
+        public static async Task EnsureExistence(this IEventSourcedReader reader, string streamName)
+        {
+            if (await reader.Exists(streamName)) return;
+            throw new InvalidOperationException($"The stream {streamName} does not exists!");
+        }
+
+        private static string GetStreamName<T>(string streamId) => StreamCategoryAttribute.GetFullStreamName<T>(streamId);
     }
 }
